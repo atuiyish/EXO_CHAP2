578b7d0567a8714f0d52cb305c8aa9f8313dd02c
atuiyish <tuyi95.at@gmail.com>
2023-09-25 22:31:06 +0200

    Chap 2

---
+++ EX08.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a,b,op;
+    int d;
+
+    system("clear");
+    printf("Indiquer les deux valeurs :\n");
+    scanf("%f",&a);
+    scanf("%f",&b);
+    system("clear");
+    printf("Indiquer l'opération :\n");
+    scanf("%d",&d);
+    system("clear");
+
+    if (d == 1)
+    {
+        op = a + b;
+        printf("%g + %g = %g\n",a,b,op);
+        getchar();
+        return 0;
+    }
+    if (d == 2)
+    {
+        op = a - b;
+        printf("%g - %g = %g\n",a,b,op);
+        getchar();
+        return 0;
+    }
+    if (d == 3)
+    {
+        op = a * b;
+        printf("%g * %g = %g\n",a,b,op);
+        getchar();
+        return 0;
+    }
+    if (d == 4)
+    {
+        op = a / b;
+        printf("%g / %g = %g\n",a,b,op);
+        getchar();
+        return 0;
+    }
+    else
+    getchar();
+    return 0;    
+}
